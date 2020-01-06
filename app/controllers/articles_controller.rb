776213f9ba118e775d6416e524ca83bba2047417
Emery Tate <tateejt@gmail.com>
2020-01-06 22:09:50 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,5 +27,8 @@ class ArticlesController < ApplicationController
 
   def update
      raise params.inspect
+     
+     @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
   end
 end
